da27ace02bc9a3d61d720152751d1868c72910d5
Dominic Chapman <dominicchapman@me.com>
2017-10-07 13:13:39 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-  #set the first key of the :railroads hash to a symbol, :pieces, whose value is the integer 4
+  # set the first key of the :railroads hash to a symbol, :pieces, whose value is the integer 4
   # set the second key of the :railroads hash to a symbol, :names, whose value is an empty hash
   # set the third key of the :railroads hash to a symbol, :rent_in_dollars, whose value is an empty hash
   monopoly = {
@@ -29,8 +29,22 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+  # set the 1st key of the :rent_in_dollars hash to a symbol, :one_piece_owned, whose value is the integer 25
+  # set the 2nd key of the :rent_in_dollars hash to a symbol, :two_pieces_owned, whose value is the integer 50
+  # set the 3rd key of the :rent_in_dollars hash to a symbol, :three_pieces_owned, whose value is the integer 100
+  # set the 4th key of the :rent_in_dollars hash to a symbol, :four_pieces_owned, whose value is the integer 200
+  monopoly = {
+    railroads: {
+      pieces: 4,
+      names: {},
+      rent_in_dollars: {
+        one_piece_owned: 25,
+        two_pieces_owned: 50,
+        three_pieces_owned: 100,
+        four_pieces_owned: 200,
+      }
+    }
+  }
 end
 
 def monopoly_with_fourth_tier
